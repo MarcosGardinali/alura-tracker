b9463808f0644827d18e5a8bb85ae10097671927
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2024-02-14 17:22:35 -0300

    New

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script lang="ts">
+import BarraLateral from './components/BarraLateral.vue';
+
+export default {
+  components: { BarraLateral },
+  data() {
+    return {
+      modoEscuroAtivo: false
+    }
+  },
+  methods: {
+    trocarTema(modoEscuroAtivo : boolean){
+      this.modoEscuroAtivo = modoEscuroAtivo
+    }
+  }
+}
+
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+    <div class="column is-one-quarter">
+      <BarraLateral @ao-tema-alterado="trocarTema"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <RouterView>
+        
+      </RouterView>
+    </div>
+  </main>
+</template>
+
+<style>
+main {
+  --bg-primario: #FFF;
+  --texto-primario: #000;
+}
+
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+
+.conteudo{
+  background-color: var(--bg-primario);
+}
+.lista {
+  padding: 1.25rem;
+}
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,88 @@
+<script lang="ts">
+import { RouterLink } from 'vue-router'
+
+export default {
+    name: 'BarraLateral',
+    methods: {
+        alterarTema() {
+            this.modoEscuroAtivo = !this.modoEscuroAtivo
+            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+        }
+    },
+    emits: ['aoTemaAlterado'],
+    data() {
+        return {
+            modoEscuroAtivo: false
+        }
+    },
+    computed: {
+        textoBotao() {
+            if (this.modoEscuroAtivo) {
+                return 'Desativar Modo Escuro'
+            } return 'Ativar Modo Escuro'
+        }
+    }
+}
+</script>
+
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="Logo do Alura-Tracker">
+        </h1>
+        <button class="button" @click="alterarTema">
+            {{ textoBotao }}
+        </button>
+        <nav class="panel mt-5">
+            <ul>
+                <router-link to="/" class="link">
+                    <li>
+                        <i class="fas fa-tasks"></i>
+                        Tarefas
+                    </li>
+                </router-link>
+                <router-link to="/projetos" class="link">
+                    <li>
+                        <i class="fas fa-project-diagram"></i>
+                        Projetos
+                    </li>
+                </router-link>
+
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+
+.panel li {
+    margin: 1rem 0.5rem;
+    text-align: start;
+    font-size: 1.2rem;
+}
+
+.link {
+    color: #fff;
+}
+
+.link:hover {
+    color: #FAF0CA;
+}
+
+.link.router-link-active {
+    color: #FAF0CA;
+}</style>
